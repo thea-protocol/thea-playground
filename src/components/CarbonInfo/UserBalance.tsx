1fb2b532b40948af123d89c5046e1d4ed357c71d
nhaga <nhaga5@gmail.com>
2023-03-13 15:29:40 +0000

    add toast to UserProfile

--- src/components/CarbonInfo/UserBalance.tsx
+++ src/components/CarbonInfo/UserBalance.tsx
@@ -1,16 +1,27 @@
 import React, { useState, useContext } from 'react'
-import { Card, CardHeader, Heading, CardBody, CardFooter, Button } from '@chakra-ui/react'
+import { Card, CardHeader, Heading, CardBody, CardFooter, Button, useToast } from '@chakra-ui/react'
 import { TheaSDKContext } from "../../components/TheaSDKProvider";
 
 function UserBalance() {
     const { theaSDK, account } = useContext(TheaSDKContext);  
     const [output, setOutput] = useState({})    
+    const toast = useToast()
+
 
     const getUsersBalance = async () => {
         console.log(account)
+        try {
           const info = await theaSDK.carbonInfo.getUsersBalance(account)
           setOutput(info)  
-      }    
+        } catch(error) {
+          toast({
+              title: 'Error',
+              description: error.toString(),
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+            })
+      }      }    
 
   return (
     <Card>
